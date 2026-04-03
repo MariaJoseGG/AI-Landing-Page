ccdcff28dc77c5ba4764f7ac2060c68c562b1fee
Jianghu <102545448+MariaJoseGG@users.noreply.github.com>
2026-04-03 09:17:12 -0500

    feat: add animations to Testimonials section (#11)

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { testimonials } from "@/constants/testimonials";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const Testimonials = () => {
   return (
@@ -12,9 +15,22 @@ export const Testimonials = () => {
           Our revolutionary AI SEO tools have transformed our clients&apos; strategies.
         </p>
 
-        <div className="overflow-hidden pt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map(testimonial => (
+        <div className="flex overflow-hidden pt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{
+              translateX: "-50%"
+            }}
+            animate={{
+              translateX: "0"
+            }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 30
+            }}
+            className="flex gap-5 flex-none pr-5"
+          >
+            {[...testimonials, ...testimonials].map(testimonial => (
               <div key={testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
                 <div className="text-lg md:text-2xl tracking-tight">
                   {testimonial.text}
@@ -38,7 +54,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
